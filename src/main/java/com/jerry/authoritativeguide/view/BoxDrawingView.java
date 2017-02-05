f23b1aed9e8e222bcdc70a55696f4e6a97b50ed8
Jerry <thrforever@126.com>
2017-02-05 14:47:43 +0800

    Chapter 29.6

--- src/main/java/com/jerry/authoritativeguide/view/BoxDrawingView.java
+++ src/main/java/com/jerry/authoritativeguide/view/BoxDrawingView.java
@@ -7,6 +7,7 @@ import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -20,11 +21,30 @@ import java.util.ArrayList;
 
 public class BoxDrawingView extends View {
 
+    private static final String TAG = "BoxDrawingView";
+
     private Box mCurrentBox;
     private ArrayList<Box> mBoxes = new ArrayList<>();
 
     private Paint mBackgroundPaint;
     private Paint mRectPaint;
+
+    enum MODE {
+        NULL, SINGLE, MULTIPLE
+    }
+
+    private MODE mMODE = MODE.NULL;
+
+    /**
+     * 初始的角度
+     */
+    private float mStartRotation;
+    /**
+     * 旋转后的角度
+     */
+    private float mEndRotation;
+
+    private float mChangeRotation;
 
     public BoxDrawingView(Context context) {
         this(context, null);
@@ -44,22 +64,37 @@ public class BoxDrawingView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         PointF pointF = new PointF(event.getX(), event.getY());
 
-        switch (event.getAction()) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
+                mMODE = MODE.SINGLE;
                 mCurrentBox = new Box(pointF);
                 mBoxes.add(mCurrentBox);
                 break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                mMODE = MODE.MULTIPLE;
+                // 两个手指落下去，获取初始角度
+                mStartRotation = getRotation(event);
+                break;
             case MotionEvent.ACTION_MOVE:
                 if (pointF != null) {
-                    mCurrentBox.setCurrent(pointF);
+                    if (mMODE == MODE.MULTIPLE) {
+                        mEndRotation = getRotation(event);
+                    } else {
+                        mCurrentBox.setCurrent(pointF);
+                    }
                     // 这里就会执行onDraw方法
                     invalidate();
                 }
                 break;
             case MotionEvent.ACTION_UP:
+                mMODE = MODE.NULL;
                 mCurrentBox = null;
                 break;
+            case MotionEvent.ACTION_POINTER_UP:
+                mMODE = MODE.SINGLE;
+                break;
             case MotionEvent.ACTION_CANCEL:
+                mMODE = MODE.NULL;
                 mCurrentBox = null;
                 break;
         }
@@ -69,6 +104,9 @@ public class BoxDrawingView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawPaint(mBackgroundPaint);
+
+        mChangeRotation = mEndRotation - mStartRotation;
+        canvas.rotate(mChangeRotation, getWidth() / 2, getHeight() / 2);
 
         for (Box box : mBoxes) {
             // 取得所有经过的点的最左、上、右、下
@@ -98,5 +136,16 @@ public class BoxDrawingView extends View {
         ArrayList list = bundle.getParcelableArrayList("boxes");
         mBoxes = (ArrayList<Box>) list.get(0);
         super.onRestoreInstanceState(superData);
+    }
+
+    /**
+     * @param event
+     * @return 获取旋转角度
+     */
+    private float getRotation(MotionEvent event) {
+        double delta_x = (event.getX(0) - event.getX(1));
+        double delta_y = (event.getY(0) - event.getY(1));
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
     }
 }
